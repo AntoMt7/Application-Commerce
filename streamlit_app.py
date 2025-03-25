e644ec8d8e6cbdfabaedc756efd02f1bdb386ea9
Antoine Magnain-Thill <antoinemt@hotmail.com>
2025-03-25 11:53:04 +0100

    Update streamlit_app.py
    Retour aux bases

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import io
 import snowflake.connector
 from snowflake.snowpark.session import Session
 import pandas as pd
-import requests
-from typing import Any, Dict, List, Optional
 
 # Connexion à Snowflake
 def get_snowflake_session():
@@ -18,17 +16,6 @@ def get_snowflake_session():
         "schema": st.secrets["snowflake"]["schema"]
     }
     return Session.builder.configs(connection_parameters).create()
-
-HOST = "TALBDQV-KI77978.snowflakecomputing.com"
-ACCOUNT = st.secrets["snowflake"]["account"]
-USER = "ANTOINE"
-PASSWORD = st.secrets["snowflake"]["password"]
-ROLE = "ACCOUNTADMIN"
-STAGE = "COMMERCE_STAGE"
-FILE = "commerce.yaml"
-WAREHOUSE = st.secrets["snowflake"]["warehouse"]
-DATABASE = st.secrets["snowflake"]["database"]
-SCHEMA = st.secrets["snowflake"]["schema"]
 
 # Fonction pour rajouter des commentaires
 def save_commentaire(nom, commentaire):
@@ -114,114 +101,10 @@ def to_csv(df):
     csv = df.to_csv(index=False)  # Convertir le DataFrame en CSV sans index
     return csv
 
-################################################################################
-#AJOUT DES DEF IA 
-###############################################################################
-def send_message(prompt: str) -> Dict[str, Any]:
-    """Calls the REST API and returns the response."""
-    request_body = {
-        "messages": [{"role": "user", "content": [{"type": "text", "text": prompt}]}],
-        "semantic_model_file": f"@{DATABASE}.{SCHEMA}.{STAGE}/{FILE}",
-    }
-    resp = requests.post(
-        url=f"https://{HOST}/api/v2/cortex/analyst/message",
-        json=request_body,
-        headers={
-            "Authorization": f'Snowflake Token="{st.session_state.CONN.rest.token}"',
-            "Content-Type": "application/json",
-        },
-    )
-    request_id = resp.headers.get("X-Snowflake-Request-Id")
-    if resp.status_code < 400:
-        return {**resp.json(), "request_id": request_id}  # type: ignore[arg-type]
-    else:
-        raise Exception(
-            f"Failed request (id: {request_id}) with status {resp.status_code}: {resp.text}"
-        )
-
-def process_message(prompt: str) -> None:
-    """Processes a message and adds the response to the chat."""
-    st.session_state.messages.append(
-        {"role": "user", "content": [{"type": "text", "text": prompt}]}
-    )
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    with st.chat_message("assistant"):
-        with st.spinner("Generating response..."):
-            response = send_message(prompt=prompt)
-            request_id = response["request_id"]
-            content = response["message"]["content"]
-            display_content(content=content, request_id=request_id)  # type: ignore[arg-type]
-    st.session_state.messages.append(
-        {"role": "assistant", "content": content, "request_id": request_id}
-    )
-
-def display_content(
-    content: List[Dict[str, str]],
-    request_id: Optional[str] = None,
-    message_index: Optional[int] = None,
-) -> None:
-    """Displays a content item for a message."""
-    message_index = message_index or len(st.session_state.messages)
-    if request_id:
-        with st.expander("Request ID", expanded=False):
-            st.markdown(request_id)
-    for item in content:
-        if item["type"] == "text":
-            st.markdown(item["text"])
-        elif item["type"] == "suggestions":
-            with st.expander("Suggestions", expanded=True):
-                for suggestion_index, suggestion in enumerate(item["suggestions"]):
-                    if st.button(suggestion, key=f"{message_index}_{suggestion_index}"):
-                        st.session_state.active_suggestion = suggestion
-        elif item["type"] == "sql":
-            with st.expander("SQL Query", expanded=False):
-                st.code(item["statement"], language="sql")
-            with st.expander("Results", expanded=True):
-                with st.spinner("Running SQL..."):
-                    df = pd.read_sql(item["statement"], st.session_state.CONN)
-                    if len(df.index) > 1:
-                        data_tab, line_tab, bar_tab = st.tabs(
-                            ["Data", "Line Chart", "Bar Chart"]
-                        )
-                        data_tab.dataframe(df)
-                        if len(df.columns) > 1:
-                            df = df.set_index(df.columns[0])
-                        with line_tab:
-                            st.line_chart(df)
-                        with bar_tab:
-                            st.bar_chart(df)
-                    else:
-                        st.dataframe(df)
-        
-################################################################################
-#                            Interface utilisateur
-###############################################################################
+# Interface utilisateur
 st.title("Application commerciale")
 session = get_snowflake_session()
-# Assurez-vous que la connexion Snowflake est active
-if 'CONN' not in st.session_state or st.session_state.CONN is None:
-    st.session_state.CONN = get_snowflake_session()
-    
 
-session_token = st.session_state.CONN._conn._rest._token
-
-# Vérifier que le token est bien récupéré
-if not session_token:
-    st.error("Impossible de récupérer le token d'authentification de Snowflake.")
-else:
-    st.success("Token d'authentification récupéré avec succès !")
-
-# Remplacez l'authentification dans votre requête API
-headers = {
-    "Authorization": f'Snowflake Token="{session_token}"',
-    "Content-Type": "application/json",
-}
-
-
-if "active_suggestion" not in st.session_state:
-    st.session_state.active_suggestion = None
-    
 # Sélection de la région
 existing_regions = get_region()
 region_choisie = st.selectbox("Sélectionner une région", existing_regions)
@@ -320,23 +203,3 @@ if secteur_choisi:
             st.write("Aucune donnée de localisation disponible pour affichage sur la carte.")
     else:
         st.write("Aucune entreprise ne correspond aux critères sélectionnés.")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-    st.session_state.suggestions = []
-    st.session_state.active_suggestion = None
-
-for message_index, message in enumerate(st.session_state.messages):
-    with st.chat_message(message["role"]):
-        display_content(
-            content=message["content"],
-            request_id=message.get("request_id"),
-            message_index=message_index,
-        )
-
-if user_input := st.chat_input("What is your question?"):
-    process_message(prompt=user_input)
-
-if st.session_state.active_suggestion:
-    process_message(prompt=st.session_state.active_suggestion)
-    st.session_state.active_suggestion = None
