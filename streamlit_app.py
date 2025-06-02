2548637f0cc2d8789fba1ead73049e5cba702204
Antoine Magnain-Thill <antoinemt@hotmail.com>
2025-06-02 14:05:19 +0200

    modifs grouped_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,8 +104,9 @@ def get_entreprises(
 
     # Grouper les entreprises par ville
     grouped_data = result.groupby(["VILLE", "LAT", "LON"]).apply(
-        lambda x: ", ".join(f"{row['NOM']} ({row['SIZE']} employés)" for _, row in x.iterrows())
-    ).reset_index(name="ENTREPRISES")
+    lambda x: ", ".join(f"{row['NOM']} ({row['SIZE']} employés)" for _, row in x.iterrows())).reset_index()
+    grouped_data.columns = ["VILLE", "LAT", "LON", "ENTREPRISES"]
+
 
     # Retourner les données
     return result.drop(columns=["LON", "LAT"]), grouped_data
